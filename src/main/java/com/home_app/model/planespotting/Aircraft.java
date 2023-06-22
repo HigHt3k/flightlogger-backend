6ad1ba0cf32596741de5e6a764f260ca7cbf153f
Töpfer, Johann <johanntoepfer@gmail.com>
2023-06-22 21:14:11 +0200

    feat: added data model for aircraft master data

---
+++ src/main/java/com/home_app/model/planespotting/Aircraft.java
@@ -0,0 +1,80 @@
+package com.home_app.model.planespotting;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name =  "aircraft")
+public class Aircraft {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "aircraft_id")
+    private Integer aircraftId;
+    @Column(name = "icao_24")
+    private String icao24;
+    @Column(name = "aircraft_registration")
+    private String aircraftRegistration;
+    @Column(name = "aircraft_manufacturer")
+    private String aircraftManufacturer;
+    @Column(name = "aircraft_type")
+    private String aircraftType;
+    @Column(name = "operator")
+    private String operator;
+    @Column(name = "operator_code")
+    private String operatorCode;
+
+    public Integer getAircraftId() {
+        return aircraftId;
+    }
+
+    public void setAircraftId(Integer aircraftId) {
+        this.aircraftId = aircraftId;
+    }
+
+    public String getIcao24() {
+        return icao24;
+    }
+
+    public void setIcao24(String icao24) {
+        this.icao24 = icao24;
+    }
+
+    public String getAircraftRegistration() {
+        return aircraftRegistration;
+    }
+
+    public void setAircraftRegistration(String aircraftRegistration) {
+        this.aircraftRegistration = aircraftRegistration;
+    }
+
+    public String getAircraftManufacturer() {
+        return aircraftManufacturer;
+    }
+
+    public void setAircraftManufacturer(String aircraftManufacturer) {
+        this.aircraftManufacturer = aircraftManufacturer;
+    }
+
+    public String getAircraftType() {
+        return aircraftType;
+    }
+
+    public void setAircraftType(String aircraftType) {
+        this.aircraftType = aircraftType;
+    }
+
+    public String getOperator() {
+        return operator;
+    }
+
+    public void setOperator(String operator) {
+        this.operator = operator;
+    }
+
+    public String getOperatorCode() {
+        return operatorCode;
+    }
+
+    public void setOperatorCode(String operatorCode) {
+        this.operatorCode = operatorCode;
+    }
+}
